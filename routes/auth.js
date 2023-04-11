6866825224e8e9548c6e8c925cc2139fbae7f7c9
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-11 11:50:03 -0700

    attempt route to register new user, broken

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@
 const Router = require("express").Router;
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
 const { SECRET_KEY } = require("../config");
 const { UnauthorizedError } = require("../expressError");
 const router = new Router();
@@ -25,5 +26,28 @@ router.post("/login", async function (req, res) {
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post("/register", async function (req, res) {
+  const { username, password, first_name, last_name, phone } = req.body;
+  console.log("req.body", req.body);
+  // const newUser = await User.register(req.body);
+
+  const newUser = await User.register({ username,
+                        password,
+                        first_name,
+                        last_name,
+                        phone });
+
+  console.log("newUser", newUser);
+  console.log("{ username, password}", { username, password});
+
+  const resp = await axios.post("/login", { username, password});
+
+  console.log("resp", resp);
+  const token = resp.data.token;
+  console.log("token", token);
+
+  return res.json({ token });
+});
+
 
 module.exports = router;
